79a7658706ca8023484e3e8c27ff87d55e1b1455
Andrew <33483980+abell95@users.noreply.github.com>
2017-11-28 02:09:06 -0800

    Create PE_Problem_2.cpp
    From PE site:
    
    "Each new term in the Fibonacci sequence is generated by adding the previous two terms. By starting with 1 and 2, the first 10 terms will be: 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...  By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms."

---
+++ PE_Problem_2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int fibonacci() {
+    int x = 1;
+    int y = 2;
+    int z = x + y;
+    int sum = 0;
+    while (z < 4000000) {
+        if (z % 2 == 0){
+           sum += z;
+        }
+        x = y;
+        y = z;
+    }
+    cout << sum << endl;
+}
+
+int main() {
+    fibonacci();
+    return 0;
+}
